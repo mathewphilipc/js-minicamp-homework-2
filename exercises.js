968071ffddf01911a0f61cc3e01cb645e1f57fef
Mathew Calkins <mathewpcalkins@gmail.com>
2017-08-04 19:44:49 -0400

    Implemented GetBiggest(x,y)

--- exercises.js
+++ exercises.js
@@ -3,6 +3,10 @@
 function getBiggest(x, y) {
   //x and y are integers.  Return the larger integer
   //if they are the same return either one
+  if (x > y) {
+    return x;
+  }
+  return y;
 }
 
 function greeting(language) {
